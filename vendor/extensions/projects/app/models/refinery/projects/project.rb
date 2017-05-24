1831fa22d8cb2a9b194a49778c6034f932f8c7d2
Logan Price <logandsprice@gmail.com>
2017-05-24 17:37:26 -0500

    Change project/images association to has_many

--- vendor/extensions/projects/app/models/refinery/projects/project.rb
+++ vendor/extensions/projects/app/models/refinery/projects/project.rb
@@ -6,7 +6,7 @@ module Refinery
 
       validates :name, :presence => true, :uniqueness => true
 
-      belongs_to :picture, :class_name => '::Refinery::Image'
+      has_many_page_images
 
       # To enable admin searching, add acts_as_indexed on searchable fields, for example:
       #
