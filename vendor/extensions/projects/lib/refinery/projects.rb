1ab8b9a1c954d74ff4551a7fbcc17534f69487a2
Logan Price <logandsprice@gmail.com>
2017-05-24 17:43:24 -0500

    Load tabs

--- vendor/extensions/projects/lib/refinery/projects.rb
+++ vendor/extensions/projects/lib/refinery/projects.rb
@@ -6,12 +6,20 @@ module Refinery
   module Projects
     require 'refinery/projects/engine'
 
+    autoload :Tab, 'refinery/projects/tabs'
+
     class << self
       attr_writer :root
+      attr_writer :tabs
 
       def root
         @root ||= Pathname.new(File.expand_path('../../../', __FILE__))
       end
+
+      def tabs
+        @tabs ||= []
+      end
+
 
       def factory_paths
         @factory_paths ||= [ root.join('spec', 'factories').to_s ]
