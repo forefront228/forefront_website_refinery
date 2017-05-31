72ee0163ef5b755c0bb257af6022fbfcbf1eb1b7
Logan Price <logandsprice@gmail.com>
2017-05-31 17:03:24 -0500

    Made associations expcept User has_many Articles

--- vendor/extensions/articles/app/models/refinery/articles/article.rb
+++ vendor/extensions/articles/app/models/refinery/articles/article.rb
@@ -6,6 +6,7 @@ module Refinery
 
       validates :title, :presence => true, :uniqueness => true
 
+      belongs_to :user, class_name: ::Refinery::Authentication::Devise::User
       # To enable admin searching, add acts_as_indexed on searchable fields, for example:
       #
       #   acts_as_indexed :fields => [:title]
--- vendor/extensions/tags/app/models/refinery/tags/tag.rb
+++ vendor/extensions/tags/app/models/refinery/tags/tag.rb
@@ -5,7 +5,8 @@ module Refinery
 
 
       validates :name, :presence => true, :uniqueness => true
-
+      has_many :taggings
+      has_many :projects, through: :taggings
       # To enable admin searching, add acts_as_indexed on searchable fields, for example:
       #
       #   acts_as_indexed :fields => [:title]
