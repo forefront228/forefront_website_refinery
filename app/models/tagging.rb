6b32ac764c371590b5a7caddea56761e2fc9b674
Logan Price <logandsprice@gmail.com>
2017-05-31 19:46:07 -0500

    Add uniqueness validation to taggings

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,4 +1,7 @@
 class Tagging < ActiveRecord::Base
+
+  validates_uniqueness_of :project_id, scope: :tag_id
+
   belongs_to :project, class_name: ::Refinery::Projects::Project
   belongs_to :tag, class_name: ::Refinery::Tags::Tag
 end
