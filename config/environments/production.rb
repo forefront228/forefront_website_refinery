b0d65f099f15022ac91f66e424388f179064c02d
Logan Price <logandsprice@gmail.com>
2017-06-19 17:15:43 -0500

    Un-comment initialize_on_precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.assets.compile = true
 
   #Supposedly for Heroku deployment
-  # config.assets.initialize_on_precompile = false
+  config.assets.initialize_on_precompile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
