eee2da3e31fec41066eef4e572042d64735ecddc
Logan Price <logandsprice@gmail.com>
2017-06-19 16:55:58 -0500

    Move initialize_on_precompile

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,8 @@ module ForefrontWebsiteRefinery
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    #Was messing with Heroku deployment
+    config.assets.initialize_on_precompile = false
   end
 end
-config.assets.initialize_on_precompile = false
