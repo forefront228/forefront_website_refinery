da02cb9231c41c7de03a1d6bbf10b897a675b4e9
Logan Price <logandsprice@gmail.com>
2017-07-12 18:40:15 -0500

    Removed methods from Translates in Image model. I don't think this breaks anything but I don't know!

--- app/models/refinery/image.rb
+++ app/models/refinery/image.rb
@@ -2,7 +2,7 @@ require 'dragonfly'
 
 module Refinery
   class Image < Refinery::Core::BaseModel
-    # translates :image_title, :image_alt
+    translates 
 
     dragonfly_accessor :image, :app => :refinery_images
 
