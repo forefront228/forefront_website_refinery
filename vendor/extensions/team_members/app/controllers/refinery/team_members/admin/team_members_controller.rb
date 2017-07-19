d42259cf70d5b6c2a79f67d760bf580cd6042870
Logan Price <logandsprice@gmail.com>
2017-07-19 16:35:26 -0500

    Add error messages to edit form

--- vendor/extensions/team_members/app/controllers/refinery/team_members/admin/team_members_controller.rb
+++ vendor/extensions/team_members/app/controllers/refinery/team_members/admin/team_members_controller.rb
@@ -6,6 +6,23 @@ module Refinery
         crudify :'refinery/team_members/team_member',
                 :title_attribute => 'first_name'
 
+        def update
+          @team_member = TeamMember.find(params[:id])
+
+          @team_member.assign_attributes(team_member_params)
+          if @team_member.valid?
+            @team_member.save
+            flash.notice = t(
+            'refinery.crudify.updated',
+            :what => "#{@team_member.first_name}"
+            )
+            create_or_update_successful
+          else
+            create_or_update_unsuccessful 'edit'
+          end
+        end
+
+
         def team_member_params
           params.require(:team_member).permit(permitted_team_member_params)
         end
