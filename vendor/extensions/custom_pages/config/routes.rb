fbee1df03989584cf9c19ed6f777046dd92a9c7a
Logan Price <logandsprice@gmail.com>
2017-07-25 17:38:39 -0500

    Remove unnecessary routes and remove links to make new CustomPage and reorder

--- vendor/extensions/custom_pages/config/routes.rb
+++ vendor/extensions/custom_pages/config/routes.rb
@@ -1,14 +1,11 @@
 Refinery::Core::Engine.routes.draw do
 
   # Frontend routes
-  namespace :custom_pages do
-    resources :custom_pages, :path => '', :only => [:index, :show]
-  end
 
   # Admin routes
   namespace :custom_pages, :path => '' do
     namespace :admin, :path => Refinery::Core.backend_route do
-      resources :custom_pages, :except => :show do
+      resources :custom_pages, :except => [:show,:create,:new,:destroy] do
         collection do
           post :update_positions
         end
