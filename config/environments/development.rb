23f8729ef85f92ab54a5d54bd13fb081fc5522bd
Logan Price <logandsprice@gmail.com>
2017-07-26 16:52:27 -0500

    Setup actionmailer email settings on production.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,8 +24,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['GMAIL_USERNAME'],
-   :password             => ENV['GMAIL_PASSWORD'],
+   :user_name            => ENV['EMAIL_USERNAME'],
+   :password             => ENV['EMAIL_PASSWORD'],
    :authentication       => "plain",
   :enable_starttls_auto => true
   }
