fa50fb23543ce44be5642df67d060e427941f6fa
Logan Price <logandsprice@gmail.com>
2017-08-02 03:43:08 -0500

    Customize CustomPage update action to set hero image to previous hero image if edit form is submitted without a hero image.

--- vendor/extensions/custom_pages/app/controllers/refinery/custom_pages/admin/custom_pages_controller.rb
+++ vendor/extensions/custom_pages/app/controllers/refinery/custom_pages/admin/custom_pages_controller.rb
@@ -6,6 +6,25 @@ module Refinery
         crudify :'refinery/custom_pages/custom_page',
                 :title_attribute => 'name'
 
+        def update
+          @custom_page.assign_attributes(custom_page_params)
+          if custom_page_params[:hero_image_id]==""
+            previous_hero_image = Refinery::CustomPages::CustomPage.find_by_id(params[:id]).hero_image_id
+            @custom_page.assign_attributes(hero_image_id: previous_hero_image)
+          end
+          
+          if @custom_page.save
+            flash.notice = t(
+              'refinery.crudify.updated',
+              :what => "'#{@custom_page.name}'"
+            )
+
+            create_or_update_successful
+          else
+            create_or_update_unsuccessful 'edit'
+          end
+        end
+
         private
 
         # Only allow a trusted parameter "white list" through.
