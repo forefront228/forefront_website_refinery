333c25631dae946abc2a466bdd2ff3cfdf9f66dd
Logan Price <logandsprice@gmail.com>
2017-08-07 11:42:43 -0500

    Move development filler seeds to run only if in Development

--- vendor/extensions/team_members/db/seeds.rb
+++ vendor/extensions/team_members/db/seeds.rb
@@ -18,24 +18,25 @@ Refinery::I18n.frontend_locales.each do |lang|
   end if defined?(Refinery::Page)
 end
 
-require 'faker'
-
 Refinery::TeamMembers::TeamMember.destroy_all
 
-#creating refinery image primary_image and secondary_image filler
-Refinery::Image.create(image:File.new('app/assets/images/team1.png'))
-Refinery::Image.create(image:File.new('app/assets/images/bg-1.jpg'))
+if Rails.env.downcase != "production"
+  require 'faker'
 
-a = Refinery::Image.create(image: File.new('app/assets/images/team-arjun.jpeg'))
-aa = Refinery::Image.create(image: File.new('app/assets/images/team-arjun2.jpeg'))
-l = Refinery::Image.create(image: File.new('app/assets/images/team-logan.jpg'))
-ll = Refinery::Image.create(image: File.new('app/assets/images/team-logan2.jpeg'))
-pav = Refinery::Image.create(image: File.new('app/assets/images/team-pavan.jpeg'))
-pavpav = Refinery::Image.create(image: File.new('app/assets/images/team-pavan2.jpg'))
+  #creating refinery image primary_image and secondary_image filler
+  Refinery::Image.create(image:File.new('app/assets/images/team1.png'))
+  Refinery::Image.create(image:File.new('app/assets/images/bg-1.jpg'))
 
-def generate_bio
-  paragraphs=[]
-  sentences = []
+  a = Refinery::Image.create(image: File.new('app/assets/images/team-arjun.jpeg'))
+  aa = Refinery::Image.create(image: File.new('app/assets/images/team-arjun2.jpeg'))
+  l = Refinery::Image.create(image: File.new('app/assets/images/team-logan.jpg'))
+  ll = Refinery::Image.create(image: File.new('app/assets/images/team-logan2.jpeg'))
+  pav = Refinery::Image.create(image: File.new('app/assets/images/team-pavan.jpeg'))
+  pavpav = Refinery::Image.create(image: File.new('app/assets/images/team-pavan2.jpg'))
+
+  def generate_bio
+    paragraphs=[]
+    sentences = []
     rand(2..4).times {
       rand(4..8).times do
         sentences << Faker::Lorem.sentence(9,true,9)
@@ -43,34 +44,35 @@ def generate_bio
       paragraphs << sentences.join("")
       sentences=[]
     }
-  paragraphs.map { |p| "<p>" + p + "</p>"}.join("")
+    paragraphs.map { |p| "<p>" + p + "</p>"}.join("")
+  end
+
+  team_members = [
+    {
+      first_name: "Logan",
+      last_name: "Price",
+      role: "Developer",
+      bio:generate_bio,
+      primary_image: l,
+      secondary_image: ll
+    },
+    {
+      first_name:"Pavan",
+      last_name:"Sarguru",
+      role:"Developer",
+      bio:generate_bio,
+      primary_image: pav,
+      secondary_image: pavpav
+    },
+    {
+      first_name:"Arjun",
+      last_name:"Venkataswamy",
+      role:"Senior Developer",
+      bio:generate_bio,
+      primary_image: a,
+      secondary_image: aa
+    }
+  ]
+
+  team_members.each { |t| Refinery::TeamMembers::TeamMember.create(t)}
 end
-
-team_members = [
-  {
-    first_name: "Logan",
-    last_name: "Price",
-    role: "Developer",
-    bio:generate_bio,
-    primary_image: l,
-    secondary_image: ll
-  },
-  {
-    first_name:"Pavan",
-    last_name:"Sarguru",
-    role:"Developer",
-    bio:generate_bio,
-    primary_image: pav,
-    secondary_image: pavpav
-  },
-  {
-    first_name:"Arjun",
-    last_name:"Venkataswamy",
-    role:"Senior Developer",
-    bio:generate_bio,
-    primary_image: a,
-    secondary_image: aa
-  }
-]
-
-team_members.each { |t| Refinery::TeamMembers::TeamMember.create(t)}
